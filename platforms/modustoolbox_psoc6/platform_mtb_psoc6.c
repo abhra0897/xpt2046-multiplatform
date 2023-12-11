f59129756ab8e4bff930a0dac3a54c2beec15448
Avra <abhra0897@gmail.com>
2023-12-11 19:38:14 +0530

    Fixed: psoc6 platform driver was changed with nr52's

--- platforms/modustoolbox_psoc6/platform_mtb_psoc6.c
+++ platforms/modustoolbox_psoc6/platform_mtb_psoc6.c
@@ -1,76 +1,57 @@
 #include <stdio.h>
-#include "platform_ncs_zephyr_nrf52.h"
+#include "platform_mtb_psoc6.h"
 
-struct spi_buf spi_tx_buf;
-const struct spi_buf_set tx = {
-    .buffers = &spi_tx_buf,
-    .count = 1
-};
-struct spi_buf spi_rx_buf;
-const struct spi_buf_set rx = {
-    .buffers = &spi_rx_buf,
-    .count = 1
-};
+/**
+ * Sharing SPI bus with display.
+ * Display driver must re-initialize spi with proper mode after each touch sampling.
+ * Comment out SPI_IS_SHARED if touch and display use seperate SPI buses. In that case,
+ * no need to re-init spi bus for display since `xpt2046_platform_spi_init()` will not be used.
+ */
 
 // Only called if SPI_IS_SHARED is defined
 void xpt2046_platform_spi_init(uint64_t spi_freq, uint8_t cpol, uint8_t cpha, uint8_t is_lsbfirst)
 {
-    spi_cfg.operation = (SPI_OP_MODE_MASTER | SPI_WORD_SET(8) |
-        (is_lsbfirst ? SPI_TRANSFER_LSB : SPI_TRANSFER_MSB) |
-        (cpol ? SPI_MODE_CPOL : 0) |
-        (cpha ? SPI_MODE_CPHA : 0));
-    spi_cfg.frequency = spi_freq;
-    spi_cfg.slave = 0;
+    cyhal_spi_init(&mSPI, SPI_MOSI, SPI_MISO, SPI_CLK, NC, NULL,
+                          8, CYHAL_SPI_MODE(cpol, cpha, is_lsbfirst), false);
+    cyhal_spi_set_frequency(&mSPI, spi_freq);
 }
-
 
 // Only called if SPI_IS_SHARED is defined
 void xpt2046_platform_spi_deinit(void)
 {
-    spi_release(spi_dev, &spi_cfg);
+    cyhal_spi_free(&mSPI);
 }
 
-// Helper function for spi transfer
-__attribute__((always_inline)) static inline void _spi_transfer(uint8_t *txdata, uint8_t *rxdata, uint8_t bytes)
-{
-    spi_tx_buf.buf = txdata;
-    spi_tx_buf.len = bytes;
-
-    spi_rx_buf.buf = rxdata;
-    spi_rx_buf.len = bytes;
-
-    spi_transceive(spi_dev, &spi_cfg, &tx, &rx);
-}
 uint8_t xpt2046_platform_spi_transfer(uint8_t data)
 {
-    uint8_t tx_data = data;
-    uint8_t rx_data;
-    _spi_transfer(&tx_data, &rx_data, 1);
-    return rx_data;
+    const uint8_t tx_buff = data;
+    uint8_t rx_buff;
+    cyhal_spi_transfer(&mSPI, &tx_buff, 1u, &rx_buff, 1u, 0);
+    return rx_buff;
 }
 
 // TODO: in xpt2046.c, remove transfer16 and use transfer instead
 uint16_t xpt2046_platform_spi_transfer16(uint16_t data)
 {
-    uint8_t tx_data[2] = {(uint8_t)(data >> 8), (uint8_t)data};
-    uint8_t rx_data[2] = {0};
-    _spi_transfer(tx_data, rx_data, 2);
-    return (((uint16_t)rx_data[0] << 8) | (uint16_t)rx_data[1]); // combine to a single uint16
+    const uint8_t tx_buff[2] = {(uint8_t)(data >> 8), (uint8_t)data};
+    uint8_t rx_buff[2];
+    cyhal_spi_transfer(&mSPI, tx_buff, 2u, rx_buff, 2u, 0);
+    return (((uint16_t)rx_buff[0] << 8) | (uint16_t)rx_buff[1]); // combine to a single uint16
 }
 
 void xpt2046_platform_delay(uint64_t ms)
 {
-    k_sleep(K_MSEC(ms));
+    cyhal_system_delay_ms(ms);
 }
 
 uint64_t xpt2046_platform_get_millis(void)
 {
-    return k_uptime_get();
+    // Define this somewhere in main application
+    // Use CysTick or relevant function from FreeRTOS
+    return (uint64_t)get_millis();
 }
 
 void xpt2046_platform_print(const char *msg)
 {
-    printk("%s", msg);
+    printf("%s", msg);
 }
-
-
